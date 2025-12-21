873e42c6fc322a9a56f5a627af972747bdc739c7
Supersheep7 <alessandro.corona.m@gmail.com>
2025-12-21 15:19:11 +0100

    .

--- utils/probe.py
+++ utils/probe.py
@@ -533,7 +533,7 @@ class Estimator:
             activations = next(iter(activations.values()))
             X = einops.rearrange(activations, 'n b d -> (n b) d').detach().cpu().numpy()
             y = einops.rearrange(labels, 'n b -> (n b)').detach().cpu().numpy()
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
             clf = Pipeline([
                 ("scaler", StandardScaler()),
